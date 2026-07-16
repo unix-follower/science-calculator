d2fd8a5b39470c0803079655f0a5040ab8859910
unix-follower <61003331+unix-follower@users.noreply.github.com>
2026-07-16 07:28:26 +0300

    Add compound growth calculator (#11)
    Co-authored-by: Artsem Nikitsenka <artsem_nikitsenka@epam.com>

--- src/test/java/org/example/sciencecalc/finance/GeneralInvestmentTest.java
+++ src/test/java/org/example/sciencecalc/finance/GeneralInvestmentTest.java
@@ -1,11 +1,14 @@
 package org.example.sciencecalc.finance;
 
 import org.example.sciencecalc.finance.GeneralInvestmentCalc.CompoundingFrequency;
+import org.example.sciencecalc.math.Constants;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GeneralInvestmentTest {
+    private static final double DELTA1 = 0.1;
     private static final double DELTA5 = 0.00001;
 
     @Test
@@ -19,5 +22,23 @@ class GeneralInvestmentTest {
         final double rate = GeneralInvestmentCalc.compoundInterestRate(initialBalance, finalBalance, term, frequency);
         // then
         assertEquals(0.051995, rate, DELTA5);
+    }
+
+    @Test
+    void testCompoundGrowth() {
+        // given
+        final short initialDeposit = 1000;
+        final double interestRate = 0.08; // 8%
+        final byte term = 20; // 20 years (240.164 months)
+        final CompoundingFrequency frequency = CompoundingFrequency.MONTHLY;
+        // when
+        final double[] results = GeneralInvestmentCalc.compoundGrowth(initialDeposit, interestRate, term, frequency);
+        // then
+        assertNotNull(results);
+        assertEquals(2, results.length);
+        final double finalBalance = results[Constants.ARR_1ST_INDEX];
+        assertEquals(4926.8, finalBalance, DELTA1);
+        final double totalCompoundGrowth = results[Constants.ARR_2ND_INDEX];
+        assertEquals(3926.8, totalCompoundGrowth, DELTA1);
     }
 }
