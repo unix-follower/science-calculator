34a78aa67048e648a1b94234c253206575f1eeba
unix-follower <61003331+unix-follower@users.noreply.github.com>
2026-07-26 14:04:37 +0300

    Add Investment Fee calculator (#14)
    Co-authored-by: Artsem Nikitsenka <artsem_nikitsenka@epam.com>

--- src/test/java/org/example/sciencecalc/finance/GeneralInvestmentTest.java
+++ src/test/java/org/example/sciencecalc/finance/GeneralInvestmentTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GeneralInvestmentTest {
     private static final double DELTA1 = 0.1;
+    private static final double DELTA2 = 0.01;
     private static final double DELTA3 = 0.001;
     private static final double DELTA5 = 0.00001;
 
@@ -79,5 +80,36 @@ class GeneralInvestmentTest {
         assertEquals(0.06952, annualDiscountRate, DELTA5);
         final double periodicDiscountRate = results[Constants.ARR_2ND_INDEX];
         assertEquals(0.00579, periodicDiscountRate, DELTA3);
+    }
+
+    @Test
+    void testInvestmentFees() {
+        // given
+        final short initialInvestmentAmount = 10_000;
+        final double salesLoad = 0.02; // 2%
+        final double annualReturn = 0.1;
+        final double annualOperatingFees = 0.02; // 2%
+        final byte investmentDuration = 10;
+        final double turnoverCost = 0.03; // 3%
+        final double redemptionFees = 0.02; // 2%
+        // when
+        final double[] results = GeneralInvestmentCalc.investmentFees(
+            initialInvestmentAmount, salesLoad, annualReturn, annualOperatingFees, investmentDuration, turnoverCost,
+            redemptionFees);
+        // then
+        assertNotNull(results);
+        assertEquals(6, results.length);
+        final double investedAmount = results[Constants.ARR_1ST_INDEX];
+        assertEquals(9800, investedAmount, DELTA1);
+        final double effectiveReturn = results[Constants.ARR_2ND_INDEX];
+        assertEquals(0.08, effectiveReturn, DELTA2);
+        final double fundValueBeforeRedemption = results[Constants.ARR_3RD_INDEX];
+        assertEquals(20_863.46, fundValueBeforeRedemption, DELTA2);
+        final double finalFundValue = results[Constants.ARR_4TH_INDEX];
+        assertEquals(20_446.2, finalFundValue, DELTA1);
+        final double fundValueWithoutFees = results[Constants.ARR_5TH_INDEX];
+        assertEquals(25_937.42, fundValueWithoutFees, DELTA2);
+        final double totalFees = results[Constants.ARR_6TH_INDEX];
+        assertEquals(5491.23, totalFees, DELTA2);
     }
 }
