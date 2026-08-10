f3f677baf93323b8b9831b2fb5c2e65fe78926c7
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-08-10 11:29:17 +0300

    Add max heart rate calculator

--- src/test/java/org/example/sciencecalc/sport/PerformanceCalcTest.java
+++ src/test/java/org/example/sciencecalc/sport/PerformanceCalcTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PerformanceCalcTest {
     private static final double DELTA1 = 0.1;
+    private static final double DELTA2 = 0.01;
 
     @Test
     void testTargetHeartRate() {
@@ -17,5 +18,55 @@ class PerformanceCalcTest {
         final double totalBurned = PerformanceCalc.targetHeartRate(age, restingHeartRate, intensityPercent);
         // then
         assertEquals(134.9, totalBurned, DELTA1);
+    }
+
+    @Test
+    void testMaxHeartRateHaskellFox() {
+        // given
+        final byte age = 32;
+        // when
+        final double maxHeartRate = PerformanceCalc.maxHeartRateHaskellFox(age);
+        // then
+        assertEquals(188, maxHeartRate, DELTA1);
+    }
+
+    @Test
+    void testMaxHeartRateInbar() {
+        // given
+        final byte age = 32;
+        // when
+        final double maxHeartRate = PerformanceCalc.maxHeartRateInbar(age);
+        // then
+        assertEquals(183.88, maxHeartRate, DELTA2);
+    }
+
+    @Test
+    void testMaxHeartRateNes() {
+        // given
+        final byte age = 32;
+        // when
+        final double maxHeartRate = PerformanceCalc.maxHeartRateNes(age);
+        // then
+        assertEquals(190.52, maxHeartRate, DELTA2);
+    }
+
+    @Test
+    void testMaxHeartRateOaklandNonlinear() {
+        // given
+        final byte age = 32;
+        // when
+        final double maxHeartRate = PerformanceCalc.maxHeartRateOaklandNonlinear(age);
+        // then
+        assertEquals(184.83, maxHeartRate, DELTA2);
+    }
+
+    @Test
+    void testMaxHeartRateTanakaMonahanSeals() {
+        // given
+        final byte age = 32;
+        // when
+        final double maxHeartRate = PerformanceCalc.maxHeartRateTanakaMonahanSeals(age);
+        // then
+        assertEquals(185.6, maxHeartRate, DELTA2);
     }
 }
